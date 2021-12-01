6910fceb9b075b601c44acb63d7d14c486bc535e
Fernando Naufel <fnaufel@gmail.com>
2021-12-01 16:46:52 -0300

    Start HTML format

--- R/bs4_book.R
+++ R/bs4_book.R
@@ -2,41 +2,34 @@
 #'
 #' Loads additional style and configs
 #'
-#' @param toc see [rmarkdown::html_document]
-#' @param toc_depth see [rmarkdown::html_document]
-#' @param toc_float see [rmarkdown::html_document]
-#' @param number_sections see [rmarkdown::html_document]
-#' @param anchor_sections see [rmarkdown::html_document]
-#' @param section_divs see [rmarkdown::html_document]
-#' @param fig_width see [rmarkdown::html_document]
-#' @param fig_height see [rmarkdown::html_document]
-#' @param fig_retina see [rmarkdown::html_document]
-#' @param fig_caption see [rmarkdown::html_document]
-#' @param dev see [rmarkdown::html_document]
-#' @param df_print see [rmarkdown::html_document]
-#' @param code_folding see [rmarkdown::html_document]
-#' @param code_download see [rmarkdown::html_document]
-#' @param self_contained see [rmarkdown::html_document]
-#' @param theme see [rmarkdown::html_document]
-#' @param highlight see [rmarkdown::html_document]
-#' @param mathjax see [rmarkdown::html_document]
-#' @param template see [rmarkdown::html_document]
-#' @param extra_dependencies see [rmarkdown::html_document]
-#' @param css see [rmarkdown::html_document]
-#' @param includes see [rmarkdown::html_document]
-#' @param keep_md see [rmarkdown::html_document]
-#' @param lib_dir see [rmarkdown::html_document]
-#' @param md_extensions see [rmarkdown::html_document]
-#' @param pandoc_args see [rmarkdown::html_document]
-#' @param ... see [rmarkdown::html_document]
-#'
 #' @export
 #'
 bs4_book <- function(
-  toc = TRUE,
-  toc_depth = 2,
-  toc_float = FALSE,
-  number_sections = TRUE,
+  theme = bs4_book_theme(
+    primary = "#086a6a",
+    base_font = font_google("Lexend"),
+    heading_font = font_google("Roboto"),
+    code_font = font_google("JetBrains Mono"),
+  ),
+  template = system.file(
+    "rmarkdown/resources/bs4_book_ptbr.html",
+    package = "bd.format"
+  ),
+  includes = rmarkdown::includes(
+    in_header = system.file(
+      "rmarkdown/resources/preamble.html",
+      package = "bd.format"
+    )
+  ),
+  css = system.file(
+    "rmarkdown/resources/styles.css",
+    package = "bd.format"
+  ),
+  repo = NULL,
+  lib_dir = "libs",
+  pandoc_args = NULL,
+  extra_dependencies = NULL,
+  split_bib = FALSE,
   anchor_sections = TRUE,
   section_divs = TRUE,
   fig_width = 7,
@@ -46,33 +39,22 @@ bs4_book <- function(
   dev = 'png',
   df_print = 'paged',
   code_folding = 'show',
-  code_download = TRUE,
-  self_contained = TRUE,
-  theme = 'readable', # https://bootswatch.com/3/readable/
   highlight = 'pygment',
   mathjax = 'default',
-  template = 'default',
-  extra_dependencies = NULL,
-  css = system.file("rmarkdown/resources/styles.css", package = "rmdformat"),
-  includes = rmarkdown::includes(
-    in_header = system.file(
-      "rmarkdown/resources/header.html",
-      package = "rmdformat"
-    )
-  ),
   keep_md = FALSE,
-  lib_dir = NULL,
   md_extensions = NULL,
-  pandoc_args = NULL,
   ...
 ) {
 
-  # call the base html_document function
-  rmarkdown::html_document(
-    toc = toc,
-    toc_depth = toc_depth,
-    toc_float = toc_float,
-    number_sections = number_sections,
+  # call the base function
+  bookdown::bs4_book(
+    theme = theme,
+    repo = repo,
+    lib_dir = lib_dir,
+    pandoc_args = pandoc_args,
+    extra_dependencies = extra_dependencies,
+    template = template,
+    split_bib = split_bib,
     anchor_sections = anchor_sections,
     section_divs = section_divs,
     fig_width = fig_width,
@@ -82,19 +64,12 @@ bs4_book <- function(
     dev = dev,
     df_print = df_print,
     code_folding = code_folding,
-    code_download = code_download,
-    self_contained = self_contained,
-    theme = theme,
     highlight = highlight,
     mathjax = mathjax,
-    template = template,
-    extra_dependencies = extra_dependencies,
     css = css,
     includes = includes,
     keep_md = keep_md,
-    lib_dir = lib_dir,
     md_extensions = md_extensions,
-    pandoc_args = pandoc_args,
     ...
   )
 
