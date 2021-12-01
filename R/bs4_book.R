21cc611bb21c837fe81e312ac6b9b3dd3be7fc00
Fernando Naufel <fnaufel@gmail.com>
2021-12-01 17:42:33 -0300

    Finish html format

--- R/bs4_book.R
+++ R/bs4_book.R
@@ -2,14 +2,44 @@
 #'
 #' Loads additional style and configs
 #'
+#' @title bs4_book_fnaufel
+#'
+#' @param theme see [bookdown::bs4_book]
+#' @param template see [bookdown::bs4_book]
+#' @param includes see [bookdown::bs4_book]
+#' @param css see [bookdown::bs4_book]
+#' @param repo see [bookdown::bs4_book]
+#' @param lib_dir see [bookdown::bs4_book]
+#' @param pandoc_args see [bookdown::bs4_book]
+#' @param extra_dependencies see [bookdown::bs4_book]
+#' @param split_bib see [bookdown::bs4_book]
+#' @param section_divs see [bookdown::bs4_book]
+#' @param fig_width see [bookdown::bs4_book]
+#' @param fig_height see [bookdown::bs4_book]
+#' @param fig_retina see [bookdown::bs4_book]
+#' @param fig_caption see [bookdown::bs4_book]
+#' @param dev see [bookdown::bs4_book]
+#' @param df_print see [bookdown::bs4_book]
+#' @param mathjax see [bookdown::bs4_book]
+#' @param keep_md see [bookdown::bs4_book]
+#' @param md_extensions see [bookdown::bs4_book]
+#' @param ... see [bookdown::bs4_book]
+#'
+#' @return  see [bookdown::bs4_book]
+#'
+#' @author fnaufel
 #' @export
 #'
-bs4_book <- function(
-  theme = bs4_book_theme(
+#' @importFrom rmarkdown includes
+#' @importFrom bslib font_google
+#' @importFrom bookdown bs4_book
+#'
+bs4_book_fnaufel <- function(
+  theme = bookdown::bs4_book_theme(
     primary = "#086a6a",
-    base_font = font_google("Lexend"),
-    heading_font = font_google("Roboto"),
-    code_font = font_google("JetBrains Mono"),
+    base_font = bslib::font_google("Lexend"),
+    heading_font = bslib::font_google("Roboto"),
+    code_font = bslib::font_google("JetBrains Mono"),
   ),
   template = system.file(
     "rmarkdown/resources/bs4_book_ptbr.html",
@@ -30,7 +60,6 @@ bs4_book <- function(
   pandoc_args = NULL,
   extra_dependencies = NULL,
   split_bib = FALSE,
-  anchor_sections = TRUE,
   section_divs = TRUE,
   fig_width = 7,
   fig_height = 5,
@@ -38,8 +67,6 @@ bs4_book <- function(
   fig_caption = TRUE,
   dev = 'png',
   df_print = 'paged',
-  code_folding = 'show',
-  highlight = 'pygment',
   mathjax = 'default',
   keep_md = FALSE,
   md_extensions = NULL,
@@ -55,7 +82,6 @@ bs4_book <- function(
     extra_dependencies = extra_dependencies,
     template = template,
     split_bib = split_bib,
-    anchor_sections = anchor_sections,
     section_divs = section_divs,
     fig_width = fig_width,
     fig_height = fig_height,
@@ -63,8 +89,6 @@ bs4_book <- function(
     fig_caption = fig_caption,
     dev = dev,
     df_print = df_print,
-    code_folding = code_folding,
-    highlight = highlight,
     mathjax = mathjax,
     css = css,
     includes = includes,
